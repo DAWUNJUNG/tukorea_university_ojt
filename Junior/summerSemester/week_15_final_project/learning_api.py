e5a61f0a357404933d9d50acb958e892b363fe65
dawun <dawun@lacy.co.kr>
2023-08-04 11:36:05 +0900

    기말과제 업로드

--- Junior/summerSemester/week_15_final_project/learning_api.py
+++ Junior/summerSemester/week_15_final_project/learning_api.py
@@ -53,13 +53,19 @@ def gpt_learning():
 
     # 크로미움 설정
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('headless')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('window-size=1920x1080')
-    # chrome_options.add_argument("disable-gpu")
+    chrome_options.add_argument("disable-gpu")
     chrome_options.add_argument('lang=ko_KR')
     chrome_options.add_argument('ignore-certificate-errors')  # SSL 관련 오류 무시
     chrome_options.add_argument('ignore-ssl-errors')  # SSL 관련 오류 무시
+
+
 
     # 셀레니움 드라이버 설정
     driver = webdriver.Chrome(service=service, options=chrome_options)
